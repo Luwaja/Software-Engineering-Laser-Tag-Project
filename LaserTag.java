795be086999444768066e8bd44c9f4047195d12b
Luke James <78236951+Luwaja@users.noreply.github.com>
2023-02-19 15:43:06 -0600

    Codename and ID boxes + createFrame method

--- LaserTag.java
+++ LaserTag.java
@@ -26,35 +26,41 @@ public class LaserTag implements ActionListener
         JFrame frame = new JFrame("LASER TAG");
         
         //Open splash screen for 3 seconds and close
-        createSplashScreen(frame).setVisible(true);
+        JWindow splashScreen = createSplashScreen(frame);
+        //splashScreen.setVisible(true);
         Thread.sleep(3000); //3 secs 
-        createSplashScreen(frame).setVisible(false);
+        splashScreen.setVisible(false);
 
         // Application starts here ------------------------------
-        // Loading JFrame window
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frame.setVisible(true);
-        frame.getContentPane().setBackground(Color.PINK);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Create frame
+        createFrame(frame);
         
         // Vertical box to hold horizontal box
-        Box vbox1 = Box.createVerticalBox();
-        Box vbox2 = Box.createVerticalBox();
+        Box vbox1name = Box.createVerticalBox();
+        Box vbox1id = Box.createVerticalBox();
+        Box vbox2name = Box.createVerticalBox();
+        Box vbox2id = Box.createVerticalBox();
         
+        // Team player names
         String[] redTeamPlayerNames = new String[20];
         String[] greenTeamPlayerNames = new String[20];
+
         // Add boxes for each teams' 19 players
         for (int i = 0; i < 20; i++)
         {
-            redTeamPlayerNames[i] =  addHorizontalBox(vbox1, "Red: Player " + i).getText();
-            greenTeamPlayerNames[i] = addHorizontalBox(vbox2, "Green: Player " + i).getText();
+            addHorizontalBox(vbox1name, "Red Player " + i);
+            addHorizontalBox(vbox1id, "ID: ");
+            addHorizontalBox(vbox2name, "Green Player " + i);
+            addHorizontalBox(vbox2id, "ID: ");
         }
         
         // Horizontal box to hold the columns
         Box hbox = Box.createHorizontalBox();
-        hbox.add(vbox1);
+        hbox.add(vbox1name);
+        hbox.add(vbox1id);
         hbox.add(Box.createHorizontalStrut(10));
-        hbox.add(vbox2);
+        hbox.add(vbox2name);
+        hbox.add(vbox2id);
 
         // Set the layout manager of the content pane to a BoxLayout
         Container contentPane = frame.getContentPane();
@@ -97,18 +103,29 @@ public class LaserTag implements ActionListener
         int centerX = (screenSize.width - splashScreen.getWidth())/2;
         int centerY = (screenSize.height - splashScreen.getHeight())/2;
         splashScreen.setLocation(centerX,centerY);
+        splashScreen.setVisible(true);
     
         return splashScreen;
+    }
+
+    public static void createFrame(JFrame frame) throws InterruptedException
+    {
+        // Loading JFrame window
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.getContentPane().setBackground(Color.GREEN);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     // Method to create and add a horizontal box to a vertical box
     private static JTextField addHorizontalBox(Box vbox, String labelText)
     {
+        //Create boxes
         Box hbox = Box.createHorizontalBox();
-        hbox.add(new JLabel(labelText));
-        hbox.add(Box.createHorizontalStrut(10));
-        JTextField textField = new JTextField(10);
-        // add action listener
+        hbox.add(new JLabel(labelText)); // Add label
+        hbox.add(Box.createHorizontalStrut(10)); // Add space for label
+        JTextField textField = new JTextField(10); // Add text field
+        // Add action listener to each text field
         LaserTag listener = new LaserTag(textField);
         textField.addActionListener(listener);
         hbox.add(textField);
@@ -131,5 +148,4 @@ public class LaserTag implements ActionListener
             System.out.println(" * Enter key pressed! Text entered: " + text);
         }
 	}
-
 }
