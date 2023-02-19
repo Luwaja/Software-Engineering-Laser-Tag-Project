c7becab3e894432464afc9f4d0bd8d497d588e59
Luke James <78236951+Luwaja@users.noreply.github.com>
2023-02-19 15:26:55 -0600

    Created separate boxes for name and ID

--- LaserTag.java
+++ LaserTag.java
@@ -15,9 +15,10 @@ import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class LaserTag
+public class LaserTag implements ActionListener
 {
-    
+    private JTextField textField;
+
     public static void main(String[] args) throws InterruptedException
     {
         // Splash screen ---------------------------------------
@@ -25,31 +26,44 @@ public class LaserTag
         JFrame frame = new JFrame("LASER TAG");
         
         //Open splash screen for 3 seconds and close
-        createSplashScreen(frame).setVisible(true);
+        JWindow splashScreen = createSplashScreen(frame);
+        //splashScreen.setVisible(true);
         Thread.sleep(3000); //3 secs 
-        createSplashScreen(frame).setVisible(false);
+        splashScreen.setVisible(false);
 
         // Application starts here ------------------------------
-        createFrame(frame);
+        // Loading JFrame window
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+        frame.getContentPane().setBackground(Color.GREEN);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         // Vertical box to hold horizontal box
-        Box vbox1 = Box.createVerticalBox();
-        Box vbox2 = Box.createVerticalBox();
+        Box vbox1name = Box.createVerticalBox();
+        Box vbox1id = Box.createVerticalBox();
+        Box vbox2name = Box.createVerticalBox();
+        Box vbox2id = Box.createVerticalBox();
         
+        // Team player names
         String[] redTeamPlayerNames = new String[20];
         String[] greenTeamPlayerNames = new String[20];
+
         // Add boxes for each teams' 19 players
         for (int i = 0; i < 20; i++)
         {
-            redTeamPlayerNames[i] =  addHorizontalBox(vbox1, "Red: Player " + i).getText();
-            greenTeamPlayerNames[i] = addHorizontalBox(vbox2, "Green: Player " + i).getText();
+            addHorizontalBox(vbox1name, "Red: Player " + i);
+            addHorizontalBox(vbox1id, "ID: " + i);
+            addHorizontalBox(vbox2name, "Green: Player " + i);
+            addHorizontalBox(vbox2id, "ID: " + i);
         }
         
         // Horizontal box to hold the columns
         Box hbox = Box.createHorizontalBox();
-        hbox.add(vbox1);
+        hbox.add(vbox1name);
+        hbox.add(vbox1id);
         hbox.add(Box.createHorizontalStrut(10));
-        hbox.add(vbox2);
+        hbox.add(vbox2name);
+        hbox.add(vbox2id);
 
         // Set the layout manager of the content pane to a BoxLayout
         Container contentPane = frame.getContentPane();
@@ -62,9 +76,9 @@ public class LaserTag
         // Output
         // Output redTeamPlayerNames to the terminal
         System.out.println("Red Team Player Names:");
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 20; i++) {              
             System.out.println(redTeamPlayerNames[i]);
-}
+        }
 
         frame.setVisible(true);
     }
@@ -92,28 +106,41 @@ public class LaserTag
         int centerX = (screenSize.width - splashScreen.getWidth())/2;
         int centerY = (screenSize.height - splashScreen.getHeight())/2;
         splashScreen.setLocation(centerX,centerY);
+        splashScreen.setVisible(true);
     
         return splashScreen;
-    }
-
-    public static void createFrame(JFrame frame) throws InterruptedException
-    {
-        // Loading JFrame window
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frame.setVisible(true);
-        frame.getContentPane().setBackground(Color.BLACK);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     // Method to create and add a horizontal box to a vertical box
     private static JTextField addHorizontalBox(Box vbox, String labelText)
     {
+        //Create boxes
         Box hbox = Box.createHorizontalBox();
-        hbox.add(new JLabel(labelText));
-        hbox.add(Box.createHorizontalStrut(10));
-        JTextField textField = new JTextField(10);
+        hbox.add(new JLabel(labelText)); // Add label
+        hbox.add(Box.createHorizontalStrut(10)); // Add space for label
+        JTextField textField = new JTextField(10); // Add text field
+        // Add action listener to each text field
+        LaserTag listener = new LaserTag(textField);
+        textField.addActionListener(listener);
         hbox.add(textField);
         vbox.add(hbox);
+        
         return textField; 
     }
+
+    public LaserTag(JTextField textField) 
+    {
+        this.textField = textField;
+    }
+
+    public void actionPerformed(ActionEvent e)
+	{
+        if (e.getSource() == textField) 
+        {
+            // Handle the event triggered by the Enter key being pressed
+            String text = textField.getText();
+            System.out.println(" * Enter key pressed! Text entered: " + text);
+        }
+	}
+
 }
