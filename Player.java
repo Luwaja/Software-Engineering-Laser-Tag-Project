6465a5946e2398fce30bc9b1fcf7f0522ef8da4a
milanjokic11 <123904845+milanjokic11@users.noreply.github.com>
2023-03-10 13:18:52 -0600

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,24 +1,75 @@
-// player class
+
+// headers
 import javax.swing.JTextField;
 
+// player class
 public class Player 
 {
-    private JTextField textField;
-    private String teamColor;
+    JTextField textFieldName;
+    JTextField textFieldID;
     String Name;
     String ID;
+    String teamColor;
+    int totalScore;
 
-    public Player(JTextField textField, String name, String id, String color)
+    public Player(JTextField textFieldName, JTextField textFieldID, String name, String id, String color)
     {
+        this.textFieldID = textFieldID;
+        this.textFieldName = textFieldName;
         this.Name = name;
         this.ID = id;
-        this.textField = textField;
         this.teamColor = color;
+        this.totalScore = 0;
+    }
+
+    public void setTextFieldName(JTextField textFieldName)
+    {
+        this.textFieldName = textFieldName;
+    }
+
+    public void setTextFieldID(JTextField textFieldID)
+    {
+        this.textFieldID = textFieldID;
+    }
+
+    public void setName(String name)
+    {
+        this.Name = name;
+    }
+
+    public void setID(String id)
+    {
+        this.ID = id;
+    }
+
+    public JTextField getTextFieldName()
+    {
+        return textFieldName;
+    }
+
+    public JTextField getTextFieldID()
+    {
+        return textFieldID;
+    }
+
+    public String getName()
+    {
+        return Name;
+    }
+
+    public String getID()
+    {
+        return ID;
+    }
+
+    public int getTotalScore()
+    {
+        return totalScore;
     }
 
     @Override 
     public String toString()
     {
-	    return "info :\n\t\t name = " + Name + "\n\t\t id = " + ID + "\n";
+	    return "info :\n\t\t name = " + Name + "\n\t\t id = " + ID + "\n\t\t team color = " + teamColor + "\n\t\t total score = " + totalScore + "\n\t\t";
     }
 }
