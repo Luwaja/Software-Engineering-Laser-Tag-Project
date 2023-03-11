cf55cca1077497c375fe5b80cb3ae43842c4ccf3
milanjokic11 <123904845+milanjokic11@users.noreply.github.com>
2023-03-11 13:41:57 -0600

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,6 +1,9 @@
 
 // headers
 import javax.swing.JTextField;
+// key listeners
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 // player class
 public class Player 
@@ -20,6 +23,24 @@ public class Player
         this.ID = id;
         this.teamColor = color;
         this.totalScore = 0;
+
+        // Add a key listener to the textFieldID
+        this.textFieldID.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    // Set the ID attribute to the value entered in the textFieldID
+                    ID = textFieldID.getText();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {}
+        });
+
     }
 
     public void setTextFieldName(JTextField textFieldName)
